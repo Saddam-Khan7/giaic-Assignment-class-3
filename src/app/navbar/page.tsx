59a3a66c59aac24379b60725a0b10fd9cd11027a
Saddam-Khan7 <saddam64672@gmail.com>
2024-11-20 03:04:45 +0500

    first commit

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <main className="p-8">
     <div className="max-w-7xl mx-auto">
       <h1 className="text-5xl font-extrabold text-gray-800 text-center mb-8">
-        Welcome to the Home Page
+        Welcome to the Navbar Page
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         <button
